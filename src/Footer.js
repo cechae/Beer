ebe118c9164f2ed688da3b237395c782dd1eb051
spicysos <cchae216@gmail.com>
2019-12-18 13:54:05 -0500

    wording change

--- src/Footer.js
+++ src/Footer.js
@@ -9,7 +9,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        BeerShop
+        BeerShop1
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
